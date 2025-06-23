4ea5477107f8c783261e1002827122588ed372b1
Junajpu <113324586+junajpu616@users.noreply.github.com>
2025-06-22 22:04:42 -0600

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="container text-center">
+        <h3>Sistema de Recursos Humanos</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
